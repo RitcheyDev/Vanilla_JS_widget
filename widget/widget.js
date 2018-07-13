81df8dcc9430c333d19f1f1de2a95a7e05424bf5
David Ritchey <ritchey0713@gmail.com>
2018-07-13 21:37:57 +0000

    add json injection to emploeesList

--- widget/widget.js
+++ widget/widget.js
@@ -2,6 +2,18 @@ var xhr = new XMLHttpRequest(); // new XML object
   xhr.onreadystatechange = function(){ // check what state the objext is at 4 means it has gotten all the info it will get back
     if(xhr.readyState === 4){
       const employees = JSON.parse(xhr.responseText)  //string on json data parse json text!
+      const statusHTML = '<ul class="bulleted">';
+        for (var i=0 i < employees.length, i+=1){
+          if(employees[i].inoffice === true){
+            statusHTML += '<li class="in">';
+          } else {
+            statusHTML += '<li class="out">';
+          }
+          statusHTML += employees[i].name;
+          statusHTML += '</li>';
+        }
+        statusHTML += '</ul>'
+        document.getElementById('employeeList').innerHTML = statusHTML
     }
   };
   xhr.open("GET", "data/data.json"); // what kind of request and the url or location for local file
